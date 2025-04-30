d8d396e9ad82d0e7328ab8ef833b32f17042a0c5
Dualisti <petra.borova630@gmail.com>
2025-04-30 13:56:31 +0200

    dffgg

--- appka/src/routes/websites/+page.svelte
+++ appka/src/routes/websites/+page.svelte
@@ -10,13 +10,11 @@
 	<span class="block w-full "></span>
 </main>
 
-<article class="justify-between items-center flex pr-150 pl-150 pb-10">	
+<article class="justify-center items-center flex pr-150 pl-150 pb-10">	
 
 
- <button	class="my-5 border-4 border-gray-700  bg-gray-500 w-[150px] h-[150px]  ease-linear hover:bg-gray-400 cursor-pointer"><FolderOpen class="w-35 size-16 stroke-2 text-white"/>  
- </button>
- <button	class="my-5 border-4 border-gray-700  bg-gray-500 w-[150px] h-[150px]  ease-linear hover:bg-gray-400 cursor-pointer"><FolderOpen class="w-36 size-16 stroke-2 text-white"/>
- </button>
+ <a href="//svelte-zeta-two.vercel.app/"> <button  class="my-5 border-4 border-gray-700  bg-gray-500 w-[150px] h-[150px]  ease-linear hover:bg-gray-400 cursor-pointer"><FolderOpen class="w-35 size-16 stroke-2 text-white"/></button></a>  
+
 
 </article>
 
@@ -28,9 +26,8 @@
                         <p class="text-white">x</p>
                         <p class="border">  </p>
                         <p class="text-white">x</p>
-                       <p class="text-center">Prvá stránka je minihra na odreagovanie, kde máte možnosť hádať vlajky a naučiť sa niečo nové. </p>
                        <p class="text-white">x</p>
-                       <p class="text-center">Druhá stránka je naša prvá prezentačná stránka viacerych projektov:</p>
+                       <p class="text-center">Naša prvá stránka je prezentačná stránka viacerych projektov:</p>
                        <p class="text-white">x</p>
                        <p class="text-center">Welcome Page</p>
                        <p class="text-center">Calculator</p>
