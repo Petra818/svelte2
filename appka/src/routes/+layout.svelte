6dde6a895482bfbe2da4464edff66b4ae411cb92
Dualisti <petra.borova630@gmail.com>
2025-05-13 08:09:21 +0200

    diagram

--- appka/src/routes/+layout.svelte
+++ appka/src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script>
 	import { assets } from '$app/paths';
 
-	import { Aperture, Menu} from '@lucide/svelte';
-
+	import { Aperture, Menu } from '@lucide/svelte';
 
 	import '../app.css';
 	import { page } from '$app/state';
@@ -10,73 +9,71 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import { afterNavigate } from '$app/navigation';
 
-
 	let { children } = $props();
-	let mobileNavOpen = $state(false)
+	let mobileNavOpen = $state(false);
 
 	afterNavigate(() => {
 		mobileNavOpen = false;
 	});
 </script>
 
-<header class="  h-15 justify-center flex border-b-2 bg-gray-400">
-<!-- MOBILE -->
-<button
+<header class="  h-15 flex justify-center border-b-2 bg-gray-400">
+	<!-- MOBILE -->
+	<button
 		class=""
 		onclick={() => {
 			mobileNavOpen = !mobileNavOpen;
-		}}><Menu /></button>
-	
- <section class=" absolute left-0 right-0 flex-col  lg:hidden {mobileNavOpen ? 'flex top-0.5 duration-1000' : 'top-[-1000px] duration-2000'}">
-	<div class=" bg-gray-300 gabarito-uniquifier w-full mt-14 fixed left-0 p-6 border-t-gray-500 border-t-2 border-b-2   ">
-		<div class=" justify-center  items-center flex">
-		<a class="hover:bg-gray-500  rounded-4xl p-3 " href="/">Domov</a>
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/aboutme">O nás</a>
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/websites">Stránky</a>
-		</div>
-			<div class="justify-center items-center flex " >	
-		<Aperture class='size-10 stroke-2 text-black '/>
-		</div>
-		<div class=" justify-center items-center flex">
-		<a  href="/projects" class=" hover:bg-gray-500  rounded-4xl p-3 " >Projekty</a> 
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/skills">Zručnosti</a>
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/contact">Kontakt</a>
-		</div>
-	   </div>
+		}}><Menu /></button
+	>
 
+	<section
+		class=" absolute left-0 right-0 flex-col lg:hidden {mobileNavOpen
+			? 'top-0.5 flex duration-1000'
+			: 'duration-2000 top-[-1000px]'}"
+	>
+		<div
+			class=" gabarito-uniquifier fixed left-0 mt-14 w-full border-b-2 border-t-2 border-t-gray-500 bg-gray-300 p-6"
+		>
+			<div class=" flex items-center justify-center">
+				<a class="rounded-4xl p-3 hover:bg-gray-500" href="/">Domov</a>
+				<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/aboutme">O nás</a>
+				<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/websites">Stránky</a>
+			</div>
+			<div class="flex items-center justify-center">
+				<Aperture class="size-10 stroke-2 text-black " />
+			</div>
+			<div class=" flex items-center justify-center">
+				<a href="/projects" class=" rounded-4xl p-3 hover:bg-gray-500">Projekty</a>
+				<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/skills">Zručnosti</a>
+				<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/contact">Kontakt</a>
+			</div>
+		</div>
 	</section>
 
 	<section class="hidden lg:flex">
-	<div class="p-3 gabarito-uniquifier space-x-7 text-2xl ">
-		<a class="hover:bg-gray-500  rounded-4xl p-3 " href="/">Domov</a>
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/aboutme">O nás</a>
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/websites">Stránky</a>
-	
-	
-	</div>
-	<div class="p-2" >	
-		<Aperture class='size-10 stroke-2 text-black'/>
-	</div>
-	<div class="p-3 gabarito-uniquifier space-x-7 text-2xl">
-		<a  href="/projects" class=" hover:bg-gray-500  rounded-4xl p-3 " >Projekty</a> 
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/skills">Zručnosti</a>
-		<a class=" hover:bg-gray-500  rounded-4xl p-3 " href="/contact">Kontakt</a>
-	</div>
+		<div class="gabarito-uniquifier space-x-7 p-3 text-2xl">
+			<a class="rounded-4xl p-3 hover:bg-gray-500" href="/">Domov</a>
+			<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/aboutme">O nás</a>
+			<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/websites">Stránky</a>
+		</div>
+		<div class="p-2">
+			<Aperture class="size-10 stroke-2 text-black" />
+		</div>
+		<div class="gabarito-uniquifier space-x-7 p-3 text-2xl">
+			<a href="/projects" class=" rounded-4xl p-3 hover:bg-gray-500">Projekty</a>
+			<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/skills">Zručnosti</a>
+			<a class=" rounded-4xl p-3 hover:bg-gray-500" href="/contact">Kontakt</a>
+		</div>
 	</section>
-
 </header>
 
-
-<main class="min-h-screen px-2 lg:px-6 mx-auto">
+<main class="mx-auto min-h-screen px-2 lg:px-6">
 	{@render children()}
 </main>
 
-
 <footer>
-	<Footer/>
+	<Footer />
 </footer>
-
-
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap');
@@ -84,7 +81,3 @@
 		background-color: rgba(255, 255, 255, 0.932);
 	}
 </style>
-
-
-
-
--- appka/src/routes/skills/+page.svelte
+++ appka/src/routes/skills/+page.svelte
@@ -1,61 +1,54 @@
 <script>
-	import { ArrowLeftCircle } from "@lucide/svelte";
-
+	import { ArrowLeftCircle } from '@lucide/svelte';
 </script>
-
 
 <main class=" ">
 	<h1 class="tulpen-one-regular p-20 text-center text-8xl">NASE ZRUCNOSTI</h1>
-	
 
-	<span class="block w-full "></span>
+	<span class="block w-full"></span>
 </main>
 
+<section class=" tomorrow-regular flex justify-center text-2xl">
+	<article class="tomorrow-regular rounded-2xl border p-7">
+		<p class="tomorrow-medium">ELEKTROTECHNIKA</p>
+		<div>Študujeme elektrotechniku na SPŠ IT-IG v Tvrdošíne s cieľom maturity a vyhlašky 21</div>
+		<div>Znalosti priemerné</div>
+		<p class="text-white">x</p>
+		<p class="border"></p>
+		<p class="text-white">x</p>
+		<p class="tomorrow-medium">WORD/EXCEL/POWERPOINT</p>
+		<div>Znalosti mierne pokročilé</div>
+		<p class="text-white">x</p>
+		<p class="border"></p>
+		<p class="text-white">x</p>
+		<p class="tomorrow-medium">PLC</p>
+		<div>Priemerne znalosti získané na praxi</div>
+		<p class="text-white">x</p>
+		<p class="border"></p>
+		<p class="text-white">x</p>
+		<p class="tomorrow-medium">PROGRAM SVELTE</p>
+		<div>Každým dňom na praxi sa zlepšujeme, zatiaľ znalosti poslabšie</div>
+		<p class="text-white">x</p>
+		<p class="border"></p>
+		<p class="text-white">x</p>
+		<p class="tomorrow-medium">PROGRAMOVACIE JAZYKY:</p>
 
- <section class=" justify-center flex tomorrow-regular text-2xl">
-
-	<article class="border tomorrow-regular rounded-2xl p-7 ">
-				<p class="tomorrow-medium">ELEKTROTECHNIKA</p>
-					<div>Študujeme elektrotechniku na SPŠ IT-IG v Tvrdošíne s cieľom maturity a vyhlašky 21 </div>
-					<div>Znalosti priemerné</div> 
-					 <p class="text-white">x</p>
-					<p class="border"></p>
-					 <p class="text-white">x</p>
-               <p class="tomorrow-medium">WORD/EXCEL/POWERPOINT</p>
-					<div>Znalosti mierne pokročilé</div> 
-					 <p class="text-white">x</p>
-					<p class="border"></p>
-					 <p class="text-white">x</p>
-				   <p class="tomorrow-medium">PLC</p>
-					<div>Priemerne znalosti získané na praxi</div>
-					 <p class="text-white">x</p> 
-					<p class="border"></p>
-					 <p class="text-white">x</p>
-				   <p class="tomorrow-medium">PROGRAM SVELTE</p>
-					<div>Každým dňom na praxi sa zlepšujeme, zatiaľ znalosti poslabšie</div> 
-					<p class="text-white">x</p>
-					<p class="border"></p>
-					<p class="text-white">x</p>	
-				   <p class="tomorrow-medium">PROGRAMOVACIE JAZYKY:</p>
-				
-					<p>HTML</p>
-					<div>Znalosti mierne pokročilé</div> 
-					<p>JAVASCRIPT</p>
-					<div>Znalosti priemerné</div> 
-					<p>CSS</p>
-					<div>Znalosti priemerné až mierne pokročilé</div> 
-			
-						
-					</article>
+		<p>HTML</p>
+		<div>Znalosti mierne pokročilé</div>
+		<p>JAVASCRIPT</p>
+		<div>Znalosti priemerné</div>
+		<p>CSS</p>
+		<div>Znalosti priemerné až mierne pokročilé</div>
+		<p class="text-white">x</p>
+		<p class="border"></p>
+		<p class="text-white">x</p>
+		<p class="tomorrow-medium">PS DIAGRAM</p>
+		<p>Znalosti priemerné získané v škole na hodine informatiky</p>
+	</article>
 </section>
-         
-     
-
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Tulpen+One&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Tomorrow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Tulpen+One&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Tomorrow:wght@500&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Tulpen+One&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Tomorrow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Tulpen+One&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Tomorrow:wght@500&display=swap');
 </style>
-
-  
